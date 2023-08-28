6ce4f7859f30e07f266b6b2c8b2a36ea6e36d7a5
Ali Tajelsir <alitajelsir@gmail.com>
2023-08-28 09:46:25 +0200

    Update styling for Git status

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -376,8 +376,8 @@
     if (( $1 )); then
       # Styling for up-to-date Git status.
       local       meta='%f'     # default foreground
-      local      clean='%163F'  # magenta foreground
-      local   modified='%228F'  # yellow foreground
+      local      clean='%80F'   # cyan foreground
+      local   modified='%229F'  # yellow foreground
       local  untracked='%216F'  # salmon foreground
       local conflicted='%196F'  # red foreground
     else
@@ -492,7 +492,7 @@
   typeset -g POWERLEVEL9K_VCS_{STAGED,UNSTAGED,UNTRACKED,CONFLICTED,COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=-1
 
   # Icon color.
-  typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_COLOR=163
+  typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_COLOR=80
   typeset -g POWERLEVEL9K_VCS_LOADING_VISUAL_IDENTIFIER_COLOR=244
   # Custom icon.
   # typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_EXPANSION='⭐'
