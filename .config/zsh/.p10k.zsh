679a45d1f03e5cfaec2241a0fa92e7111a648eb6
Ali Tajelsir <alitajelsir@gmail.com>
2023-08-28 15:50:42 +0200

    Update styling for Git status

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -376,9 +376,9 @@
     if (( $1 )); then
       # Styling for up-to-date Git status.
       local       meta='%f'     # default foreground
-      local      clean='%80F'   # cyan foreground
-      local   modified='%229F'  # yellow foreground
-      local  untracked='%216F'  # salmon foreground
+      local      clean='%80F'   # turquoise foreground
+      local   modified='%144F'  # navajowhite foreground
+      local  untracked='%244F'  # grey foreground
       local conflicted='%196F'  # red foreground
     else
       # Styling for incomplete and stale Git status.
