c4b7f5c6fece6503c3c9c16d23c99c33406e4fa0
Ali Tajelsir <alitajelsir@gmail.com>
2023-08-31 08:37:31 +0200

    Set environment variables through profile.d instead of systemd

---
+++ .config/profile.d/env-var.sh
@@ -0,0 +1,19 @@
+append_path "$HOME/.local/bin"
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
+export ZDOTDIR=$XDG_CONFIG_HOME/zsh
+export EDITOR=nvim
+export FFMPEG_DATADIR=$XDG_CONFIG_HOME/ffmpeg
+export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
+export GTK_USE_PORTAL=1
+export LESS="$LESS --tabs 2"
+export LIBVA_DRIVER_NAME=iHD
+export MANPAGER="sh -c 'col -bx | bat -pl man --theme=Monokai\ Extended'"
+export SSH_ASKPASS='/usr/bin/ksshaskpass'
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+export VISUAL=nvim
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    export MOZ_ENABLE_WAYLAND=1
+fi
