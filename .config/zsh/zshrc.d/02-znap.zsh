c18cf7fa2ec51faa6fdb7ed1386313d9c5f737ad
Ali Tajelsir <alitajelsir@gmail.com>
2023-08-31 21:56:11 +0200

    Update znap initialization function

--- .config/zsh/zshrc.d/02-znap.zsh
+++ .config/zsh/zshrc.d/02-znap.zsh
@@ -3,6 +3,8 @@
 		znap_local=$XDG_DATA_HOME/znap/zsh-snap
 		znap_remote=https://github.com/marlonrichert/zsh-snap.git
 
-	[[ -r $znap_local/znap.zsh ]] || git clone --depth 1 $znap_remote $znap_local
+	[[ -r $znap_local/znap.zsh ]] ||
+		git clone --depth 1 $znap_remote $znap_local
 	source $znap_local/znap.zsh
 }
+
