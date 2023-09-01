8b7b9c62d41c677c2d05d816f8083e96e36a96fb
Ali Tajelsir <alitajelsir@gmail.com>
2023-09-01 15:51:56 +0200

    set environment variables through profile.d instead of .zshenv

--- .config/profile.d/env-vars.sh
+++ .config/profile.d/env-vars.sh
@@ -1,12 +1,12 @@
-typeset -U path
-path=($path $HOME/.local/bin)
+export PATH=$PATH:$HOME/.local/bin
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
-ZDOTDIR=$XDG_CONFIG_HOME/zsh
 export EDITOR=nvim
 export FFMPEG_DATADIR=$XDG_CONFIG_HOME/ffmpeg
 export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
 export LESS="--tab=2 $LESS"
 export MANPATH=:$PREFIX/share/man:$XDG_DATA_HOME/man
+export MANROFFOPT=-c
+export ZDOTDIR=$XDG_CONFIG_HOME/zsh
