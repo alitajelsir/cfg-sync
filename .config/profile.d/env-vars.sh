086b02b540bb3ad5a3d8a8f19b01b064dc2b5d14
Ali Tajelsir <alitajelsir@gmail.com>
2023-09-04 04:10:37 +0200

    Update environment variables

--- .config/profile.d/env-vars.sh
+++ .config/profile.d/env-vars.sh
@@ -15,5 +15,5 @@ export SSH_ASKPASS=/usr/bin/ksshaskpass
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
 export VISUAL=nvim
 export ZDOTDIR=$XDG_CONFIG_HOME/zsh
-[ $XDG_SESSION_TYPE != wayland ] ||
+[ "$XDG_SESSION_TYPE" != "wayland" ] ||
 	export MOZ_ENABLE_WAYLAND=1
