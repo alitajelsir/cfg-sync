56fedee0b7fa2898934fa02d0d90b52ce950b3c6
Ali Tajelsir <alitajelsir@gmail.com>
2023-09-06 12:08:33 +0200

    Update environment variables

--- .config/profile.d/env-vars.sh
+++ .config/profile.d/env-vars.sh
@@ -6,8 +6,6 @@ export XDG_STATE_HOME=$HOME/.local/state
 export EDITOR=nvim
 export FFMPEG_DATADIR=$XDG_CONFIG_HOME/ffmpeg
 export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
-export LESS="$LESS -x2"
-export MANPAGER='col -bx | bat -pl man --theme=Monokai\ Extended'
+export LESS="$LESS -x2RF"
 export MANPATH=:$PREFIX/share/man:$XDG_DATA_HOME/man
-export MANROFFOPT=-c
 export ZDOTDIR=$XDG_CONFIG_HOME/zsh
