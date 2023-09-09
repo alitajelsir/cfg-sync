cdafc768be7cd5c9badb335646ea5ebcc88eb049
Ali Tajelsir <alitajelsir@gmail.com>
2023-09-09 21:51:00 +0200

    Upate zsh configuration

--- .config/zsh/zshrc.d/04-commands.zsh
+++ .config/zsh/zshrc.d/04-commands.zsh
@@ -1,5 +1,6 @@
 # Configure aliases
 () {
+
 	# List aliases
 	local -a commands=(
 		aria2c="aria2c --dir $HOME/Downloads"
@@ -13,7 +14,6 @@
 		help=run-help
 		qsenc='ffmpeg -init_hw_device qsv=hw -filter_hw_device hw'
 		kudl="ydl -o '%(playlist)s/%(chapter_number)02d-%(chapter)s/%(playlist_index)02d-%(title)s.%(ext)s'"
-		ls=exa
 		mv='mv -iv'
 		pvc='protonvpn-cli c -f'
 		pvd='protonvpn-cli d'
@@ -37,26 +37,25 @@ alias -g -- --help='--help 2>&1 | col -bx | bat -pl help --theme=Monokai\ Extend
 # Configure functions
 # Command not found handler
 command_not_found_handler() {
-	local purple='\e[1;35m' bright='\e[0;1m' green='\e[1;32m' reset='\e[0m'
-	printf 'zsh: command not found: %s\n' "$1"
-	local entries=(
-		${(f)"$(/usr/bin/pacman -F --machinereadable -- "/usr/bin/$1")"}
-	)
+	local entries=( ${(f)"$(/usr/bin/pacman -F --machinereadable -- "/usr/bin/$1")"} )
+	local red='\e[31;1m' green='\e[1;32m' magenta='\e[1;35m' bright='\e[0;1m' reset='\e[0m'
+
+	printf "zsh: command not found: ${red}$1${reset}\n" "$1"
+
 	if (( ${#entries[@]} )); then
 		printf "${bright}$1${reset} may be found in the following package(s):\n"
+
 		local pkg
-		for entry in "${entries[@]}"
-		do
-			local fields=(
-				${(0)entry}
-			)
+		for entry in "${entries[@]}"; do
+			local fields=( ${(0)entry} )
 			if [[ "$pkg" != "${fields[2]}" ]]; then
-				printf "${purple}%s/${bright}%s ${green}%s${reset}\n" "${fields[1]}" "${fields[2]}" "${fields[3]}"
+				printf "${magenta}%s/${bright}%s ${green}%s${reset}\n" "${fields[1]}" "${fields[2]}" "${fields[3]}"
 			fi
-			printf '    /%s\n' "${fields[4]}"
+			printf '  /%s\n' "${fields[4]}"
 			pkg="${fields[2]}"
 		done
 	fi 1>&2
+
 	return 127
 }
 
