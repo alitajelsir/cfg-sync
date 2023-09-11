80d293999cb9ad0f352ce05b572c780ea6109f6b
Ali Tajelsir <alitajelsir@gmail.com>
2023-09-11 22:15:24 +0200

    Update environment variables

--- .config/profile.d/env-vars.sh
+++ .config/profile.d/env-vars.sh
@@ -6,6 +6,8 @@ export XDG_STATE_HOME=$HOME/.local/state
 export EDITOR=nvim
 export FFMPEG_DATADIR=$XDG_CONFIG_HOME/ffmpeg
 export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
+export GREP_COLORS='mt=30;103'
 export LESS="$LESS -x2RF"
 export MANPATH=:$PREFIX/share/man:$XDG_DATA_HOME/man
+export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/rg/config
 export ZDOTDIR=$XDG_CONFIG_HOME/zsh
