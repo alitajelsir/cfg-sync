2a8c02ed70960c0f8ba216eb217d491cd193b0f6
Ali Tajelsir <alitajelsir@gmail.com>
2023-09-12 16:12:11 +0200

    Update zsh commands

--- .config/zsh/zshrc.d/04-commands.zsh
+++ .config/zsh/zshrc.d/04-commands.zsh
@@ -3,7 +3,10 @@
 
 	# List aliases
 	local -a commands=(
+		%=
+		\$=
 		aria2c="aria2c --dir $HOME/Downloads"
+		-='cd -'
 		cfg="git --git-dir $XDG_CONFIG_HOME/cfg-sync --work-tree $HOME"
 		cp='cp -iv'
 		eza='eza -abF --icons --group-directories-first'
@@ -12,22 +15,22 @@
 		ffprobe='ffprobe -hide_banner'
 		grep='grep --color=auto'
 		help=run-help
-		qsenc='ffmpeg -init_hw_device qsv=hw -filter_hw_device hw'
 		kudl="ydl -o '%(playlist)s/%(chapter_number)02d-%(chapter)s/%(playlist_index)02d-%(title)s.%(ext)s'"
 		ls='ls -AFhv --color=auto --group-directories-first'
 		mv='mv -iv'
 		pvc='protonvpn-cli c -f'
 		pvd='protonvpn-cli d'
 		pvs='protonvpn-cli s'
+		qsenc='ffmpeg -init_hw_device qsv=hw -filter_hw_device hw'
 		rm='rm -Iv'
 		rsync='rsync -hP'
 		sudo='sudo '
+		ydla='yt-dlp -x -f ba/b'
 		ydl=yt-dlp
-		ydla='ydl -x -f ba/b'
 	)
 
 	# Set aliases
-	alias $commands
+	alias -- $commands
 }
 
 # Set global aliases
@@ -38,21 +41,21 @@ alias -g -- --help='--help 2>&1 | col -bx | bat -pl help --theme=Monokai\ Extend
 # Configure functions
 # Command not found handler
 command_not_found_handler() {
-	local entries=( ${(f)"$(/usr/bin/pacman -F --machinereadable -- "/usr/bin/$1")"} )
+	local entries=( ${(f)"$(pacman -F --machinereadable -- /usr/bin/$1)"} )
 	local red='\e[31;1m' green='\e[1;32m' magenta='\e[1;35m' bright='\e[0;1m' reset='\e[0m'
 
-	printf "zsh: command not found: ${red}$1${reset}\n" "$1"
+	printf "zsh: command not found: $red$1$reset\n" $1
 
-	if (( ${#entries[@]} )); then
-		printf "${bright}$1${reset} may be found in the following package(s):\n"
+	if (( $#entries )); then
+		printf "$bright$1$reset may be found in the following package(s):\n"
 
 		local pkg
-		for entry in "${entries[@]}"; do
+		for entry in $entries; do
 			local fields=( ${(0)entry} )
-			[[ $pkg = ${fields[2]} ]] ||
-				printf "${magenta}%s/${bright}%s ${green}%s${reset}\n" "${fields[1]}" "${fields[2]}" "${fields[3]}"
-			printf '  /%s\n' "${fields[4]}"
-			pkg="${fields[2]}"
+			[[ $pkg = $fields[2] ]] ||
+				printf "$magenta%s/$bright%s $green%s$reset\n" $fields[1] $fields[2] $fields[3]
+			printf '  /%s\n' $fields[4]
+			pkg=$fields[2]
 		done
 	fi 1>&2
 
