fb4d7c2aa4868dd67f227fd84e1f37d645e789b2
Ali Tajelsir <alitajelsir@gmail.com>
2023-10-30 17:06:25 +0200

    Update environment variables
    Divide environment variables into multiple files

---
+++ .config/profile.d/03-locale.sh
@@ -0,0 +1,2 @@
+export LC_COLLATE=C.UTF-8
+export LC_CTYPE=en_US.UTF-8
--- .config/profile.d/04-applications.sh
+++ .config/profile.d/04-applications.sh
@@ -1,8 +1,3 @@
-export PATH=$PATH:$HOME/.local/bin
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_DATA_HOME=$HOME/.local/share
-export XDG_STATE_HOME=$HOME/.local/state
 export CUDA_CACHE_PATH=$XDG_CACHE_HOME/nvidia
 export EDITOR=nvim
 export FFMPEG_DATADIR=$XDG_CONFIG_HOME/ffmpeg
@@ -10,8 +5,6 @@ export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
 export GNUPGHOME=$XDG_DATA_HOME/gnupg
 export GTK2_RC_FILES=$XDG_CONFIG_HOME/gtk-2.0/settings.ini
 export GTK_USE_PORTAL=1
-export LC_COLLATE=C.UTF-8
-export LC_CTYPE=en_US.UTF-8
 export LESS="$LESS -x2RF"
 export LIBVA_DRIVER_NAME=iHD
 export MANPAGER="sh -c 'col -bx | bat -pl man --theme=Monokai\ Extended'"
@@ -22,6 +15,5 @@ export SSH_ASKPASS_REQUIRE=prefer
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
 export VISUAL=nvim
 export XZ_DEFAULTS=-T0
-export ZDOTDIR=$XDG_CONFIG_HOME/zsh
 [ "$XDG_SESSION_TYPE" != "wayland" ] ||
 	export MOZ_ENABLE_WAYLAND=1
