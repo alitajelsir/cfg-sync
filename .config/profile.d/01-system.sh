641837b76e7d5bd1ed9a94eb49b1822d690f2dc2
Ali Tajelsir <alitajelsir@gmail.com>
2023-11-23 11:48:21 +0200

    Update profile.d environment variables

--- .config/profile.d/01-system.sh
+++ .config/profile.d/01-system.sh
@@ -1,4 +1,15 @@
+#	Locale
+export LC_COLLATE=C.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+# PATH
+export PATH=$PATH:$HOME/.local/bi
+
+# XDG dirs
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
+
+# Shell
+ZDOTDIR=$HOME/.config/zsh
