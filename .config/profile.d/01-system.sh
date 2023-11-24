bd85c110cf6a492dda01edd2261fe040e01ed1bd
Ali Tajelsir <alitajelsir@gmail.com>
2023-11-24 13:54:49 +0200

    Update profile.d drop-in environment variables

---
+++ .config/profile.d/01-system.sh
@@ -0,0 +1,12 @@
+# Configure locale
+export LC_COLLATE=C.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+# Append PATH
+export PATH=$PATH:$HOME/.local/bin
+
+# Set XDG user directories
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
