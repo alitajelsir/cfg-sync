f5ab2d0fb1614665814ac6b0f41e4d8eb2ec48db
Ali Tajelsir <alitajelsir@gmail.com>
2024-03-28 03:38:34 +0200

    Update zsh configuration

---
+++ .config/zsh/zshrc.d/02-prompt.zsh
@@ -0,0 +1,11 @@
+# Use instant prompt
+[[ ! -r $XDG_CACHE_HOME/p10k-instant-prompt-${(%):-%n}.zsh ]] ||
+	source $XDG_CACHE_HOME/p10k-instant-prompt-${(%):-%n}.zsh
+
+# Set prompt theme
+[[ ! -r $ZDOTDIR/.p10k.zsh ]] ||
+	source $ZDOTDIR/.p10k.zsh
+
+# Set cursor shape to bar
+_cursor_bar() { printf '\e[6 q' }
+precmd_functions+=(_cursor_bar)
