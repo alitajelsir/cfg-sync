30dfcaf3377968eefd217ab7904f85c86fa7178a
Ali Tajelsir <alitajelsir@gmail.com>
2025-12-05 12:57:35 +0200

    Update profile.d configuration

--- .config/profile.d/02-applications.sh
+++ .config/profile.d/02-applications.sh
@@ -7,5 +7,7 @@ export MANPATH=:$PREFIX/share/man:$XDG_DATA_HOME/man
 export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/rg/config
 export XZ_DEFAULTS='-T0'
 
-[[ $UID == 0 ]] ||
+if [[ $UID != 0 ]]; then
+	fd . -t l --max-depth=1 $HOME -X rm
 	ln -sf /storage/emulated/0/* $HOME
+fi
